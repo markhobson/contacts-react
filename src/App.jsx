024e8b62d205df3eb25ca3ebb3d0c4b2230f0053
Mark Hobson <mark.hobson@blackpepper.co.uk>
2019-02-07 17:00:36 +0000

    Use function components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import {AppBar, Toolbar, Typography} from "@material-ui/core";
 
-class App extends React.Component {
-	render() {
-		return (
-			<AppBar>
-				<Toolbar>
-					<Typography variant="h6" color="inherit">
-						Contacts
-					</Typography>
-				</Toolbar>
-			</AppBar>
-		);
-	}
+function App() {
+	return (
+		<AppBar>
+			<Toolbar>
+				<Typography variant="h6" color="inherit">
+					Contacts
+				</Typography>
+			</Toolbar>
+		</AppBar>
+	);
 }
 
 export default App;
