e98fffa0adefc1d0445aaca66892cfa060162b69
Mark Hobson <mark.hobson@blackpepper.co.uk>
2019-02-14 11:32:07 +0000

    Show snackbar message when contact saved

--- src/ContactForm.jsx
+++ src/ContactForm.jsx
@@ -1,31 +1,51 @@
 import React from "react";
-import {Button, Grid, TextField} from "@material-ui/core";
+import {Button, Grid, IconButton, Snackbar, TextField} from "@material-ui/core";
+import CloseIcon from "@material-ui/icons/Close";
 
 class ContactForm extends React.Component {
 	
 	constructor(props) {
 		super(props);
-		this.state = {contact: Object.assign({}, props.contact)};
+		this.state = {
+			contact: Object.assign({}, props.contact),
+			snackbar: {
+				open: false,
+				message: ''
+			}
+		};
 	}
 	
 	render() {
 		return (
-			<form onSubmit={(event) => this.handleSubmit(event)}>
-				<Grid container direction="column" alignItems="flex-end" spacing={32}>
-					{/* TODO: Surely there's a better way to make a grid item full width */}
-					<Grid item style={{width: '100%'}}>
-						<TextField
-							label="Name"
-							value={this.state.contact.name}
-							onChange={event => this.handleNameChange(event)}
-							fullWidth
-						/>
+			<React.Fragment>
+				<form onSubmit={(event) => this.handleSubmit(event)}>
+					<Grid container direction="column" alignItems="flex-end" spacing={32}>
+						{/* TODO: Surely there's a better way to make a grid item full width */}
+						<Grid item style={{width: '100%'}}>
+							<TextField
+								label="Name"
+								value={this.state.contact.name}
+								onChange={event => this.handleNameChange(event)}
+								fullWidth
+							/>
+						</Grid>
+						<Grid item>
+							<Button type="submit" variant="contained" color="primary">Save</Button>
+						</Grid>
 					</Grid>
-					<Grid item>
-						<Button type="submit" variant="contained" color="primary">Save</Button>
-					</Grid>
-				</Grid>
-			</form>
+				</form>
+				<Snackbar
+					open={this.state.snackbar.open}
+					message={this.state.snackbar.message}
+					action={
+						<IconButton color="inherit" onClick={event => this.handleSnackbarClose(event)}>
+							<CloseIcon/>
+						</IconButton>
+					}
+					autoHideDuration={3000}
+					onClose={event => this.handleSnackbarClose(event)}
+				/>
+			</React.Fragment>
 		);
 	}
 	
@@ -36,7 +56,12 @@ class ContactForm extends React.Component {
 	
 	handleSubmit(event) {
 		this.props.onSave(this.state.contact);
+		this.setState({snackbar: {open: true, message: 'Contact saved'}});
 		event.preventDefault();
+	}
+	
+	handleSnackbarClose(event) {
+		this.setState(state => ({snackbar: {...state.snackbar, open: false}}));
 	}
 }
 
