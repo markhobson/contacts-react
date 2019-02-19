d0ac7ce17a0c3c35f8f20b655ee37159e07b8ec6
Mark Hobson <mark.hobson@blackpepper.co.uk>
2019-02-19 11:06:31 +0000

    Use Formik's higher order component

--- src/ContactForm.jsx
+++ src/ContactForm.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {Button, Grid, TextField} from "@material-ui/core";
-import {Formik} from "formik";
+import {withFormik} from "formik";
 import ConfirmationDialog from "./ConfirmationDialog";
 
 class ContactForm extends React.Component {
@@ -15,94 +15,56 @@ class ContactForm extends React.Component {
 	}
 	
 	render() {
+		const {values, errors, handleChange, isValid, handleSubmit} = this.props;
+		
 		return (
-			<Formik
-				initialValues={this.initialValues()}
-				isInitialValid={true}
-				validate={values => this.validate(values)}
-				onSubmit={values => this.handleSubmit(values)}
-			>
-				{({values, errors, handleChange, isValid, handleSubmit}) => (
-					<form onSubmit={handleSubmit}>
-						<Grid container direction="column" spacing={32}>
-							<Grid item>
-								<TextField
-									label="Name"
-									name="name"
-									value={values.name}
-									onChange={handleChange}
-									helperText={errors.name || ''}
-									error={errors.name && errors.name.length > 0}
-									fullWidth
-									required
-								/>
-							</Grid>
-							<Grid item>
-								<TextField
-									type="email"
-									label="Email"
-									name="email"
-									value={values.email}
-									onChange={handleChange}
-									helperText={errors.email || ''}
-									error={errors.email && errors.email.length > 0}
-									fullWidth
-								/>
-							</Grid>
-							<Grid item container justify="flex-end" spacing={16}>
-								<Grid item>
-									<Button onClick={event => this.handleDelete(event)}>Delete</Button>
-									<ConfirmationDialog
-										open={this.state.dialog.open}
-										title={`Delete ${this.toName(this.props.contact)}`}
-										content="Are you sure?"
-										confirm="Delete"
-										onConfirm={event => this.handleDeleteConfirm(event)}
-										onCancel={event => this.handleDeleteCancel(event)}
-									/>
-								</Grid>
-								<Grid item>
-									<Button type="submit" variant="contained" color="primary" disabled={!isValid}>
-										Save
-									</Button>
-								</Grid>
-							</Grid>
+			<form onSubmit={handleSubmit}>
+				<Grid container direction="column" spacing={32}>
+					<Grid item>
+						<TextField
+							label="Name"
+							name="name"
+							value={values.name}
+							onChange={handleChange}
+							helperText={errors.name || null}
+							error={errors.name && errors.name.length > 0}
+							fullWidth
+							required
+						/>
+					</Grid>
+					<Grid item>
+						<TextField
+							type="email"
+							label="Email"
+							name="email"
+							value={values.email}
+							onChange={handleChange}
+							helperText={errors.email || null}
+							error={errors.email && errors.email.length > 0}
+							fullWidth
+						/>
+					</Grid>
+					<Grid item container justify="flex-end" spacing={16}>
+						<Grid item>
+							<Button onClick={event => this.handleDelete(event)}>Delete</Button>
+							<ConfirmationDialog
+								open={this.state.dialog.open}
+								title={`Delete ${this.toName(this.props.contact)}`}
+								content="Are you sure?"
+								confirm="Delete"
+								onConfirm={event => this.handleDeleteConfirm(event)}
+								onCancel={event => this.handleDeleteCancel(event)}
+							/>
 						</Grid>
-					</form>
-				)}
-			</Formik>
+						<Grid item>
+							<Button type="submit" variant="contained" color="primary" disabled={!isValid}>
+								Save
+							</Button>
+						</Grid>
+					</Grid>
+				</Grid>
+			</form>
 		);
-	}
-	
-	initialValues() {
-		return {
-			name: this.props.contact.name,
-			email: this.props.contact.email
-		};
-	}
-	
-	validate(values) {
-		const errors = {};
-
-		if (values.name.length === 0) {
-			errors.name = 'Everyone needs a name.';
-		}
-		
-		if (values.email.indexOf('@') === -1) {
-			errors.email = `This doesn't look like an email address.`;
-		}
-		
-		return errors;
-	}
-	
-	handleSubmit(values) {
-		const contact = {
-			id: this.props.contact.id,
-			name: values.name,
-			email: values.email,
-			avatar: this.props.contact.avatar
-		};
-		this.props.onSave(contact);
 	}
 	
 	handleDelete(event) {
@@ -123,4 +85,39 @@ class ContactForm extends React.Component {
 	}
 }
 
-export default ContactForm;
+const FormikContactForm = withFormik({
+	
+	mapPropsToValues: (props) => ({
+		name: props.contact.name,
+		email: props.contact.email
+	}),
+	
+	isInitialValid: () => true,
+	
+	validate: (values) => {
+		const errors = {};
+		
+		if (values.name.length === 0) {
+			errors.name = 'Everyone needs a name.';
+		}
+		
+		if (values.email.indexOf('@') === -1) {
+			errors.email = `This doesn't look like an email address.`;
+		}
+		
+		return errors;
+	},
+
+	handleSubmit: (values, formikBag) => {
+		const {props} = formikBag;
+		const contact = {
+			id: props.contact.id,
+			name: values.name,
+			email: values.email,
+			avatar: props.contact.avatar
+		};
+		props.onSave(contact);
+	}
+})(ContactForm);
+
+export default FormikContactForm;
