29fde44c33b702034922810d31546702678a96e2
Mark Hobson <mark.hobson@blackpepper.co.uk>
2019-02-19 14:46:38 +0000

    Order contacts by name

--- src/ContactList.jsx
+++ src/ContactList.jsx
@@ -5,14 +5,17 @@ import ContactListItem from "./ContactListItem";
 function ContactList(props) {
 	const isSelected = contact => contact.id === (props.selectedContact && props.selectedContact.id);
 	
-	const listItems = props.contacts.map(contact =>
-		<ContactListItem
-			key={contact.id}
-			contact={contact}
-			selected={isSelected(contact)}
-			onSelect={() => props.onSelect(contact)}
-		/>
-	);
+	const listItems = props.contacts
+		.concat()
+		.sort((contact1, contact2) => contact1.name.localeCompare(contact2.name))
+		.map(contact =>
+			<ContactListItem
+				key={contact.id}
+				contact={contact}
+				selected={isSelected(contact)}
+				onSelect={() => props.onSelect(contact)}
+			/>
+		);
 	
 	return (
 		<List>{listItems}</List>
